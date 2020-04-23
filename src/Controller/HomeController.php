b9185af6d93411cda2c6959ec1846dc7b7e821ad
Cedric Flamain <cedflam@gmail.com>
2020-04-23 16:43:21 +0200

    Create form login

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/home.html.twig');
+    }
+
+    /**
+     * Permet de se connecter
+     *
+     * @Route("/login", name="account_login")
+     *
+     * @param AuthenticationUtils $utils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        return $this->render('home/home.html.twig', [
+            'hasError' => $error,
+            'username' => $username
+        ]);
+    }
+
+    /**
+     * Permet de se deconnecter
+     * @Route("/logout", name="account_logout")
+     */
+    public function logout()
+    {
+        //géré par Symfony
+    }
+}
